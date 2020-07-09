600a60e32fe33e9c7dc7098a171f1598456ebfd1
OhhWear <thepirateking3d2y@gmail.com>
2020-07-09 16:02:06 +0530

    Changed left margin on li

--- components/Styled/Navbar/NavItem.tsx
+++ components/Styled/Navbar/NavItem.tsx
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export default styled.li`
     margin: 0 30px;
-    margin-left: ${props => props.end ? "auto":"10px"};
+    margin-left: ${props => props.end ? "auto":"30px"};
 `;
