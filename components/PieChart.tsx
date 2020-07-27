25676cb9045577c5fcb3b04839a98247ce993515
OhhWear <thepirateking3d2y@gmail.com>
2020-07-27 20:58:24 +0530

    Organised some imports

--- components/PieChart.tsx
+++ components/PieChart.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react'
 import styled from 'styled-components';
 import { ResponsivePie } from '@nivo/pie'
-import { Spin } from 'antd';
 
 interface Props {
 	portfolio: {
--- components/StockSeachBar.tsx
+++ components/StockSeachBar.tsx
@@ -1,5 +1,5 @@
+import React, { FC } from 'react';
 import { Select } from 'antd';
-import { FC } from 'react';
 
 const { Option } = Select;
 
